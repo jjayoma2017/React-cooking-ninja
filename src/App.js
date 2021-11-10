4e6f59624adc3c6aadff65a510b7c8fd9cb033fc
jjayoma <jjayoma@its.jnj.com>
2021-11-10 16:31:22 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// page components
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
+import Navbar from './components/Navbar'
+
+// styles
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/recipes/:id" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
